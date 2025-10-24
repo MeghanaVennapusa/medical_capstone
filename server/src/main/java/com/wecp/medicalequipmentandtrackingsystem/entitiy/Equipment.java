7ac4f1215ed64fc4b5840f8fff6f867edbef8359
username <wecp-dummy@gmail.com>
2025-10-24 13:26:38 +0000

    Equipment entity

--- server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/entitiy/Equipment.java
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/entitiy/Equipment.java
@@ -5,5 +5,45 @@ import javax.persistence.*;
 
 @Table(name = "equipments") // do not change table name
 public class Equipment {
-
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String description;
+    @ManyToOne
+    Hospital hospital;
+    public Equipment(String name, String description, Hospital hospital) {
+        this.name = name;
+        this.description = description;
+        this.hospital = hospital;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Hospital getHospital() {
+        return hospital;
+    }
+    public void setHospital(Hospital hospital) {
+        this.hospital = hospital;
+    }
+    
+    
+    
+    
+    }
+    
