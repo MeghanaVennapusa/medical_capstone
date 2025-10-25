1e9f7bf126314274b1e28f29c83058b65791ad43
username <wecp-dummy@gmail.com>
2025-10-25 10:50:50 +0000

    added httpservice for maintenance shcedule

--- client/src/services/http.service.ts
+++ client/src/services/http.service.ts
@@ -25,4 +25,19 @@ export class HttpService {
   getMaintenanceById(maintenanceId: number): Observable<any> {
     return this.http.get<any>(`${this.baseUrl}/api/technician/maintenance/${maintenanceId}`);
   }
+
+  // Get all hospitals
+getAllHospitals(): Observable<any[]> {
+  return this.http.get<any[]>(`${this.baseUrl}/api/hospitals`);
+}
+
+// Get equipment by hospital ID
+getEquipmentByHospital(hospitalId: number): Observable<any[]> {
+  return this.http.get<any[]>(`${this.baseUrl}/api/equipment/hospital/${hospitalId}`);
+}
+
+// Schedule maintenance
+scheduleMaintenance(data: any): Observable<any> {
+  return this.http.post<any>(`${this.baseUrl}/api/technician/maintenance`, data);
+}
 }
