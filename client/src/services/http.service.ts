ca3dbd546b7c1d1c6e1575857a573025a84b4412
username <wecp-dummy@gmail.com>
2025-10-25 12:22:41 +0000

    Order ts

--- client/src/services/http.service.ts
+++ client/src/services/http.service.ts
@@ -8,7 +8,17 @@ import { AuthService } from './auth.service';
   providedIn: 'root'
 })
 export class HttpService {
-  public serverName=environment.apiUrl;
-//todo: complete missing code.. 
+
+  public serverName = environment.apiUrl;
+
+  constructor(private http:HttpClient){}
+
+  getOrders():Observable<any>{
+    return this.http.get<any>(`${this.serverName}/orders`);
+  }
+
+  updateOrderStatus(orderId:number,newStatus:string):Observable<any>{
+    return this.http.put<any>(`${this.serverName}/order/update/${orderId}`,{status:newStatus});
+  }
   
 }
