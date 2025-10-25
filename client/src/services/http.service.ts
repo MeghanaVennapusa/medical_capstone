b83e50dff04b51fc12ad481602ee87ea0d8c32cc
username <wecp-dummy@gmail.com>
2025-10-25 12:34:38 +0000

    service

--- client/src/services/http.service.ts
+++ client/src/services/http.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment.development';
-import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-  public serverName=environment.apiUrl;
-//todo: complete missing code.. 
-  
+  private baseUrl = `${environment.apiUrl}`;
+
+  constructor(private http: HttpClient) {}
+
+  // Get all maintenance records
+  getAllMaintenances(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/api/technician/maintenance`);
+  }
+
+  // Update maintenance by ID
+  updateMaintenance(maintenanceId: number, maintenance: any): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/api/technician/maintenance/update/${maintenanceId}`, maintenance);
+  }
+
+  // Optional: Get maintenance by ID (if needed later)
+  getMaintenanceById(maintenanceId: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/api/technician/maintenance/${maintenanceId}`);
+  }
 }
